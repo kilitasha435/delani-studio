2f2c5f27e66bc580ac262297b798088e3be6acee
kevin-kili <kilikevin435@gmail.com>
2020-02-22 06:03:39 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function){
-    $()
+    $("#tit1").hide();
+    $("#tit2").hide();
+    $("#tit3").hide();
+    $("#des").hide();
+    $("#dev").hide();
+    $("#pro").hide();
 }
+
+
+$(document).ready(function() {
+    $('#design').click(function() {
+        $('img#icon1').toggle();
+        $('label#icon1').toggle();
+        $('h5#tit1').toggle();
+        $('p#des').toggle();
+    });
+});
+
+$(document).ready(function() {
+    $('#dev').click(function() {
+        $('img#icon2').toggle();
+        $('label#icon2').toggle();
+        $('h5#tit2').toggle();
+        $('p#dev').toggle();
+    });
+});
