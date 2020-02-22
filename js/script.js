d72f1353a5a76c2effd21e5a89bd41a9113aa1c1
kevin-kili <kilikevin435@gmail.com>
2020-02-22 06:26:51 +0300

    add hover

--- js/script.js
+++ js/script.js
@@ -22,6 +22,91 @@ $(document).ready(function() {
         $('img#icon2').toggle();
         $('label#icon2').toggle();
         $('h5#tit2').toggle();
-        $('p#dev').toggle();
+        $('p#devl').toggle();
+    });
+});
+
+$(document).ready(function() {
+    $('#pro').click(function() {
+        $('img#icon3').toggle();
+        $('label#icon3').toggle();
+        $('h5#tit3').toggle();
+        $('p#prod').toggle();
+    });
+});
+
+$(document).ready(function() {
+    $('#contact').submit(function(event) {
+        var myName = $('#name').val();
+        var myEmail = $('#email').val();
+        var comment = $('#textarea').val();
+
+        if (myName === '' || myEmail === '' || comment === '') {
+            alert('Please fill in the required data');
+        } else {
+            alert(myName + ' ,we have received your message.Thank you for reaching out to us.');
+        }
+        event.preventDefault();
+        $('#name').val('');
+        $('#email').val('');
+        $('#textarea').val('');
+    });
+});
+
+$(document).ready(function() {
+    $('#kwork4').mouseover(function() {
+        $('#work4').show();
+    });
+    $('#kwork4').mouseleave(function() {
+        $('#work4').hide();
+    });
+
+    $('#kwork3').mouseover(function() {
+        $('#work3').show();
+    });
+    $('#kwork3').mouseleave(function() {
+        $('#work3').hide();
+    });
+
+    $('#kwork2').mouseover(function() {
+        $('#work2').show();
+    });
+    $('#kwork2').mouseleave(function() {
+        $('#work2').hide();
+    });
+
+    $('#kwork1').mouseover(function() {
+        $('#work1').show();
+    });
+    $('#kwork1').mouseleave(function() {
+        $('#work1').hide();
+    });
+
+    $('#kwork5').mouseover(function() {
+        $('#work5').show();
+    });
+    $('#kwork5').mouseleave(function() {
+        $('#work5').hide();
+    });
+
+    $('#kwork6').mouseover(function() {
+        $('#work6').show();
+    });
+    $('#kwork6').mouseleave(function() {
+        $('#work6').hide();
+    });
+
+    $('#kwork7').mouseover(function() {
+        $('#work7').show();
+    });
+    $('#kwork7').mouseleave(function() {
+        $('#work7').hide();
+    });
+
+    $('#kwork8').mouseover(function() {
+        $('#work8').show();
+    });
+    $('#kwork8').mouseleave(function() {
+        $('#work8').hide();
     });
 });
